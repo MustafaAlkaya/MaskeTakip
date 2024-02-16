ee870d9420c209a9c3678f6920642c33e67bcc57
Mustafa Alkaya <info.mustafaalkaya@gmail.com>
2024-02-16 17:11:47 +0300

    Add project files.

---
+++ Workaround/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using Entities.Concrete;
+using System.Xml.Linq;
+
+//NewMethod();
+
+Citizen citizen = new();
+citizen.Name = "Mustafa";
+citizen.LastName = "Alkaya";
+citizen.BirthDay = 1999;
+citizen.NationalIdentity = 51998751742; 
+
+
+
+Console.ReadLine();
+
+static void NewMethod()
+{
+    string message = "Selam";
+    double amount = 1000.5;
+    int number = 100;
+    bool isJoin = false;
+
+    Console.WriteLine(message);
+}
